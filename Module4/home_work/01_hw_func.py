9a3088db296473c3e49884a3f22d7e5d24a9494f
BlackDow81 <106910022+BlackDow81@users.noreply.github.com>
2022-06-24 14:07:26 +0300

    Update 01_hw_func.py

--- Module4/home_work/01_hw_func.py
+++ Module4/home_work/01_hw_func.py
@@ -1,11 +1,15 @@
-# Дан шестизначный номер билета. Определить, является ли билет счастливым.
-# Решение реализовать в виде функции.
-# Билет считается счастливым, если сумма его первых и последних цифр равны.
-
 def lucky_ticket(ticket_number):
-    # TODO: your code here
-    pass
+    ticket_number = str(ticket_number)
+    sum1 = int(ticket_number[0]) + int(ticket_number[1]) + int(ticket_number[2])
+    sum2 = int(ticket_number[3]) + int(ticket_number[4]) + int(ticket_number[5])
+    if sum1 == sum2:
+        print("your ticket is lucky! :)")
+    else:
+        print("your ticket is not lucky :(")
 
+
+ticket_number = input("please enter six numbers: ")
+print(lucky_ticket(ticket_number))
 
 # Тестируем функцию
 print(lucky_ticket(123006))
