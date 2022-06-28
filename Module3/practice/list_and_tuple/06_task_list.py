21c60e9d2a92f05ea3780329de2803868e45c09d
BlackDow81 <106910022+BlackDow81@users.noreply.github.com>
2022-06-28 20:08:44 +0300

    Update 06_task_list.py

--- Module3/practice/list_and_tuple/06_task_list.py
+++ Module3/practice/list_and_tuple/06_task_list.py
@@ -5,3 +5,11 @@ first_number = int(input())     # Первое число
 second_number = int(input())    # Второе число
 
 # TODO: your code here
+first_number = int(input("please enter a first number: "))  # Первое число
+second_number = int(input("please enter a second number: "))  # Второе число
+num_list = []
+multiple_of_3 = 0
+for number in range(first_number, second_number + 1):
+    if number % 3 == 0:
+        num_list.append(number)
+print("here is the list of numbers multiple of 3: ", num_list)
